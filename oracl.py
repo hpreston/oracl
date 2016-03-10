84a4e18174c906ae30124b5e0304f95326ec409c
Hank Preston <hank.preston@gmail.com>
2016-03-10 16:01:38 -0500

    Couple basic adds

--- oracl.py
+++ oracl.py
@@ -1,1 +1,26 @@
 __author__ = 'hapresto'
+
+
+from flask import Flask
+import socket
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    sysname = socket.gethostname()
+
+    return 'Hello World! from ' + sysname
+
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser('Sample Application')
+    parser.add_argument(
+        '-p', '--port', help='Port to listen on ', required=False, default=5000)
+    args = parser.parse_args()
+    app.secret_key = '1234'
+    port = int(args.port)
+    app.run(host='0.0.0.0', port=port, debug=True)
